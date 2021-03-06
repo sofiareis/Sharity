09570e8a3ee644d27a9312c526e0961cbc62b266
emilyl <emily.lukas@gmail.com>
2021-03-06 12:03:33 -0800

    add basic navigation

---
+++ frontend/Sharity/screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native';
+
+function LoginScreen({ naviagtion }) {
+  return (
+    <View>
+      <Text>Hello landing</Text>    
+    </View>
+  );
+}
+
+export default LoginScreen;
