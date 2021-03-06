41ddce46fe4e11349663b5d7f88b8ab76afe9346
emilyl <emily.lukas@gmail.com>
2021-03-06 14:09:34 -0800

    fix map syntax

--- frontend/Sharity/screens/User/MapViewScreen.js
+++ frontend/Sharity/screens/User/MapViewScreen.js
@@ -8,29 +8,27 @@ import {
   TouchableOpacity,
   Dimensions
 } from 'react-native';
-
-import {StyleSheet} from 'react-native';
-
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 function MapViewScreen({ navigation }) {
-  <View style={styles.mapcontainer}>
-   <MapView
-       provider={PROVIDER_GOOGLE}
-       style={styles.map}
-       initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-       showUserLocation={true} >
-       <Marker coordinate={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-       }}  />
-   </MapView>
-</View>
+  return (
+    <View style={styles.mapcontainer}>
+     <MapView
+         provider={PROVIDER_GOOGLE}
+         style={styles.map}
+         initialRegion={{
+           latitude: 37.78825,
+           longitude: -122.4324,
+           latitudeDelta: 0.015,
+           longitudeDelta: 0.0121,
+         }}
+         showUserLocation={true} >
+         <Marker coordinate={{
+           latitude: 37.78825,
+           longitude: -122.4324,
+         }}  />
+     </MapView>
+   </View>);
 }
 
 const styles = StyleSheet.create({
