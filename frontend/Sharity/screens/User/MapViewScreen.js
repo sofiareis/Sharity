7b19ec3ba8d4ec83e34dc813ad873fb1a9bf505a
Sofia Reis <sofiareis2018@gmail.com>
2021-03-06 14:00:48 -0800

    maps

--- frontend/Sharity/screens/User/MapViewScreen.js
+++ frontend/Sharity/screens/User/MapViewScreen.js
@@ -9,15 +9,40 @@ import {
   Dimensions
 } from 'react-native';
 
+import {StyleSheet} from 'react-native';
+
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+
 function MapViewScreen({ navigation }) {
-  return (
-    <View>
-      <Text>Browse Map</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('UserBrowse')}>
-        <Text>Browse View</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  <View style={styles.mapcontainer}>
+   <MapView
+       provider={PROVIDER_GOOGLE}
+       style={styles.map}
+       initialRegion={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+       showUserLocation={true} >
+       <Marker coordinate={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+       }}  />
+   </MapView>
+</View>
 }
+
+const styles = StyleSheet.create({
+  mapcontainer: {
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+  },
+  map: {
+        ...StyleSheet.absoluteFillObject,
+  },
+})
 
 export default MapViewScreen;
