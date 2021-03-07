f496cd59c89948b91f44d0b492018d69c9b486dd
emilyl <emily.lukas@gmail.com>
2021-03-06 18:54:58 -0800

    some backend changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var firebase = require("firebase/app");
+require("firebase/firestore");
 const port = 3000;
 const app = express();
+
+
+
+firebase.initializeApp(firebaseConfig);
+//firebase.firestore()
+//firebase.auth()
 
 app.use(bodyParser.json())
 
